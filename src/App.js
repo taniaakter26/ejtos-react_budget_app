848f092a9a7ea206de7be475a31c977bb31af480
taniaakter26 <53605115+taniaakter26@users.noreply.github.com>
2023-04-10 23:16:22 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { AppProvider } from './context/AppContext';
+import CartValue from './components/CartValue';
+import Remaining from './component/Remaining';
+import ExpenseTotal from './component/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ItemSelected from './components/ItemSelected';
+import Location from './components/Location';
+
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'> 
+                    <div className='col-sm'>
+                        <CartValue />
+                    </div>
+                    <div className='col-sm'>
+                        <Location />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change Allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <ItemSelected/>
+                    </div>
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
